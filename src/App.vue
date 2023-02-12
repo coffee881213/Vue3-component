e4c4fb907492d9b99cc68ec0fdd2c3515cd4f895
coffee881213 <1092841490@qq.com>
2023-02-12 19:56:21 +0800

    有vue3-loading-overlay样式

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <List />
+</template>
+  
+<script>
+  import ListVue from "./components/List.vue";
+  export default {
+    name: "App",
+    components: {
+      List: ListVue,
+    },
+  };
+</script>
+  
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+  
---
+++ src/components/List.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+      <div v-for="(item, index) in list" :key="index" :style="{ background: item.background, width: '100%', height: '500px' }">
+        {{index}}
+      </div>
+      <vue3-loading-overlay v-if="loading" />
+      <!-- 
+      <div v-if="loading" style="width: 100%; height: 500px;">Loading...</div>
+      -->
+    </div>
+  </template>
+  
+  <script>
+  //需要在VSCode终端运行命令npm install vue3-loading-overlay
+  import { vue3LoadingOverlay } from 'vue3-loading-overlay';
+  
+  export default {
+    components: {
+      vue3LoadingOverlay
+    },
+    data() {
+      return {
+        list: [{ background: 'rgb(233,32,38)' }],
+        loading: false,
+        numOfDivs: 0
+      };
+    },
+    methods: {
+      getList(num) {
+        this.loading = true;
+        const delay = Math.random() * 5000;
+        setTimeout(() => {
+           // generate an array of random colors
+          const color1 = [{ background: 'rgb(233,32,38)' }];
+          const colors_add = Array.from(Array(num-1), () => {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            return { background: `rgb(${r}, ${g}, ${b})` };
+          });
+          // add the new colors to the existing list
+          const colors = [...color1, ...colors_add];
+          
+          this.list = [...this.list, ...colors];
+          this.numOfDivs += 10;
+          this.loading = false;
+        }, delay);
+      }
+    },
+    created() {
+      this.getList(10);
+    },
+    mounted() {
+      window.addEventListener('scroll', () => {
+        if (this.numOfDivs < 50 && window.scrollY > window.innerHeight * 0.5) {
+          this.getList(10);
+        }
+      });
+    }
+  };
+  </script>
+  
+  <style>
+  
+  </style>
+  
